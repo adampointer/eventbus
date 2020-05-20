7075efffb6ed40db76fe72acb1bb5887f0e4c188
Adam Pointer <adam.pointer@gmx.com>
2020-05-20 14:43:43 +0100

    First commit

---
+++ eventbus.go
@@ -0,0 +1,131 @@
+package eventbus
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Event interface{}
+
+type Topic string
+
+type EventBus struct {
+	lock   sync.RWMutex
+	topics map[Topic]*Subscriptions
+}
+
+func NewEventBus() *EventBus {
+	return &EventBus{topics: make(map[Topic]*Subscriptions)}
+}
+
+func (b *EventBus) Subscribe(topic Topic) chan Event {
+	c := make(chan Event, 1)
+	sub := b.get(topic)
+	if sub != nil {
+		sub.Add(c)
+	} else {
+		sub := NewSubscriptions()
+		sub.Add(c)
+		b.set(topic, sub)
+	}
+	return c
+}
+
+func (b *EventBus) Unsubscribe(topic Topic, c chan Event) error {
+	sub := b.get(topic)
+	if sub != nil {
+		if err := sub.Delete(c); err != nil {
+			return err
+		}
+		if sub.Len() == 0 {
+			b.delete(topic)
+		}
+		return nil
+	}
+	return fmt.Errorf("unknown event type '%s'", topic)
+}
+
+func (b *EventBus) Publish(topic Topic, e Event) {
+	sub := b.get(topic)
+	if sub != nil {
+		sub.Publish(e)
+	}
+}
+
+func (b *EventBus) get(topic Topic) *Subscriptions {
+	b.lock.RLock()
+	defer b.lock.RUnlock()
+	if sub, ok := b.topics[topic]; ok {
+		return sub
+	}
+	return nil
+}
+
+func (b *EventBus) set(topic Topic, sub *Subscriptions) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	b.topics[topic] = sub
+}
+
+func (b *EventBus) delete(topic Topic) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	delete(b.topics, topic)
+}
+
+type Subscriptions struct {
+	lock        sync.RWMutex
+	subscribers map[chan Event]struct{}
+}
+
+func NewSubscriptions() *Subscriptions {
+	return &Subscriptions{subscribers: make(map[chan Event]struct{})}
+}
+
+func (s *Subscriptions) Add(c chan Event) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.subscribers[c] = struct{}{}
+}
+
+func (s *Subscriptions) Delete(c chan Event) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if _, ok := s.subscribers[c]; !ok {
+		return errors.New("attempting to remove unknown subscriber")
+	}
+	delete(s.subscribers, c)
+	return nil
+}
+
+func (s *Subscriptions) Len() int {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	return len(s.subscribers)
+}
+
+func (s *Subscriptions) Publish(e Event) {
+	var wg sync.WaitGroup
+	s.lock.RLock()
+	wg.Add(len(s.subscribers))
+	for c := range s.subscribers {
+		go func(ch chan Event) {
+			defer s.recoverClosedChannel(ch, &wg)
+			ch <- e
+		}(c)
+	}
+	s.lock.RUnlock()
+	wg.Wait()
+}
+
+func (s *Subscriptions) recoverClosedChannel(c chan Event, wg *sync.WaitGroup) {
+	if r := recover(); r != nil {
+		log.Println("channel is closed")
+		if err := s.Delete(c); err != nil {
+			log.Printf("unable to remove closed channel from subscriptions")
+		}
+	}
+	wg.Done()
+}
---
+++ eventbus_test.go
@@ -0,0 +1,124 @@
+package eventbus
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEventBus_Subscribe(t *testing.T) {
+	bus := NewEventBus()
+	var ev1 Topic = "foo"
+	var ev2 Topic = "bar"
+
+	_ = bus.Subscribe(ev1)
+	_ = bus.Subscribe(ev1)
+	_ = bus.Subscribe(ev2)
+
+	assert.Len(t, bus.topics, 2)
+	assert.Len(t, bus.topics[ev1].subscribers, 2)
+	assert.Len(t, bus.topics[ev2].subscribers, 1)
+}
+
+func TestEventBus_Unsubscribe(t *testing.T) {
+	bus := NewEventBus()
+	var ev1 Topic = "foo"
+
+	c := bus.Subscribe(ev1)
+
+	assert.Len(t, bus.topics, 1)
+	assert.Len(t, bus.topics[ev1].subscribers, 1)
+
+	assert.NoError(t, bus.Unsubscribe(ev1, c))
+
+	assert.Len(t, bus.topics, 0)
+}
+
+func TestEventBus_Publish(t *testing.T) {
+	bus := NewEventBus()
+	var ev1 Topic = "foo"
+	e := struct{ foo string }{"bar"}
+	c := bus.Subscribe(ev1)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		select {
+		case evt := <-c:
+			assert.Equal(t, e, evt)
+		case <-time.After(time.Second):
+			assert.Fail(t, "test timed out")
+		}
+		wg.Done()
+	}()
+	bus.Publish(ev1, e)
+	wg.Wait()
+}
+
+func TestEventBus_Concurrency(t *testing.T) {
+	bus := NewEventBus()
+	var ev1 Topic = "foo"
+	e := struct{ foo string }{"bar"}
+	n := 10
+	var wg sync.WaitGroup
+	wg.Add(n)
+	chans := make([]chan Event, n)
+
+	for i := 0; i < n; i++ {
+		c := bus.Subscribe(ev1)
+		chans[i] = c
+
+		go func() {
+			select {
+			case evt := <-c:
+				assert.Equal(t, e, evt)
+			case <-time.After(time.Second):
+				assert.Fail(t, "test timed out")
+			}
+			wg.Done()
+		}()
+	}
+	bus.Publish(ev1, e)
+	wg.Wait()
+
+	for _, c := range chans {
+		go func(ch chan Event) {
+			assert.NoError(t, bus.Unsubscribe(ev1, ch))
+		}(c)
+	}
+}
+
+func TestEventBus_ClosedChannel(t *testing.T) {
+	bus := NewEventBus()
+	var ev1 Topic = "foo"
+	e := struct{ foo string }{"bar"}
+	n := 10
+	var wg sync.WaitGroup
+	wg.Add(n)
+	chans := make([]chan Event, n)
+
+	for i := 0; i < n; i++ {
+		c := bus.Subscribe(ev1)
+		chans[i] = c
+
+		go func() {
+			select {
+			case evt := <-c:
+				if evt != nil {
+					assert.Equal(t, e, evt)
+				}
+			case <-time.After(time.Second):
+				assert.Fail(t, "test timed out")
+			}
+			wg.Done()
+		}()
+	}
+	close(chans[5])
+	bus.Publish(ev1, e)
+	wg.Wait()
+
+	assert.Len(t, bus.topics[ev1].subscribers, n-1)
+}
